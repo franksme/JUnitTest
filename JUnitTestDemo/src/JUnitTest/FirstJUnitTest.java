ad6559f2fc3649880fc26f957bc494212fff8a6f
Frank Fazil <frankfazil.sme@gmail.com>
2018-04-19 12:37:17 -0700

    First commit

---
+++ JUnitTestDemo/src/JUnitTest/FirstJUnitTest.java
@@ -0,0 +1,20 @@
+package JUnitTest;
+
+import org.junit.Test;
+
+public class FirstJUnitTest {
+
+	
+	
+		@Test
+		public void loginApplication() {
+		System.out.println("Hello, this is my first TestNG Test and it's logged in the application");
+			
+		}
+		
+		@Test
+		public void selectApplication() {
+		System.out.println("OK, the application is selected");
+			
+}
+}
